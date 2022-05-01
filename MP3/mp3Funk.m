063e6887ee40ffcb8786d2dd146b6ad76c805579
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-01 18:18:43 +0200

    s

--- MP3/mp3Funk.m
+++ MP3/mp3Funk.m
@@ -3,8 +3,8 @@ function dy = mp3Funk(t,y)
 dy = [y(2); t * y(2) - y(1)^2];
 
 [m,mDeriv] = mass(t);
-
-%xBiss = -c/m * sqrt(xPrim^2 + yPrim^2) * xPrim + mDeriv/m * Ux
-%xBiss = -c/m * sqrt(xPrim^2 + yPrim^2) * yPrim + mDeriv/m * Uy - g
+U = uTFunk(t);
+%xBiss = -c/m * sqrt(xPrim^2 + yPrim^2) * xPrim + mDeriv/m * U(1)
+%xBiss = -c/m * sqrt(xPrim^2 + yPrim^2) * yPrim + mDeriv/m * U(2) - g
 
 end
