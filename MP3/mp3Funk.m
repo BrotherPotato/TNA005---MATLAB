3fae01510c4bfce376fb1f6222da4d2e35cd8155
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-04 19:53:37 +0200

    s

--- MP3/mp3Funk.m
+++ MP3/mp3Funk.m
@@ -1,10 +1,12 @@
-function [dx,dy]  = mp3Funk(t,xy)
+function dxy  = mp3Funk(t,xy)
 
+c = 0.25; % kg/m
+g = 9.82; % m/s^2
 %dy = [y(2); t * y(2) - y(1)^2];
 %dx = x''(t)        x(2) = x'
 [m,mDeriv] = mass(t);
 U = uTFunk(t);
-dx = -c/m * sqrt((xy(2))^2 + (xy(4))^2) * xy(2) + mDeriv/m * U(1);
-dy = -c/m * sqrt((xy(2))^2 + (xy(4))^2) * xy(4) + mDeriv/m * U(2) - g;
+dxy(1) = -c/m * sqrt((xy(2))^2 + (xy(4))^2) * xy(2) + mDeriv/m * U(1);
+dxy(2) = -c/m * sqrt((xy(2))^2 + (xy(4))^2) * xy(4) + mDeriv/m * U(2) - g;
 
 end
