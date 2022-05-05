872b40725003a7188a43ced6a35b918dcb86e02c
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-05 13:38:46 +0200

    s

--- MP3/mass.m
+++ MP3/mass.m
@@ -1,12 +1,12 @@
-function [m,mDeriv] = mass(t)
+function [m,mD] = mass(t)
 % goes from 40 kg to 20 kg during 40 s
 m = 40;
 if 0 <= t && t <= 40 
     m = 40 - 0.5 * t;
-    mDeriv = -0.5;
+    mD = -0.5;
 elseif t > 40
     m = 20;
-    mDeriv = 0;
+    mD = 0;
 end
 m;
 end
