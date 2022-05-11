5229e7d7e394b1c71171f85132696bb443c6a092
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-12 00:19:29 +0200

    s

--- MP3/minRK4.m
+++ MP3/minRK4.m
@@ -2,19 +2,10 @@ function [t,y] = minRK4(fun, tspan, y1, n) % anpassa (:,1) , undersök globalt t
 a = tspan(1);
 b = tspan(2);
 t = linspace(a, b, n+1);
-%h = t(2) - t(1);
 h = (b-a)/n;
 y = zeros(length(y1),length(t));
 
 y(1:length(y1)) = y1;
-
-% for i=1:n
-%     k1 = fun(t(:,i), y(:,1));
-%     k2 = fun(t(:,i) + h/2, y(:,i) + h * k1/2);
-%     k3 = fun(t(:,i) + h/2, y(:,i) + h * k2/2);
-%     k4 = fun(t(:,i) + h, y(:,i) + h * k3);
-%     y(:,1+i) = y(:,1) + h * (k1 + 2 * k2 + 2 * k3 + k4) /6;
-% end
 
 for i = 1:n
     k1 = fun(t(:,i),y(:,i));
