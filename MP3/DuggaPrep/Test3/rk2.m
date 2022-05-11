47365e1fb1c82f560917328e0338f086879a89b9
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-11 12:03:51 +0200

    s

---
+++ MP3/DuggaPrep/Test3/rk2.m
@@ -0,0 +1,15 @@
+function [t,y] = rk2(fun,tspan, ybv, n)
+
+a = tspan(1);
+b = tspan(2);
+t = linspace(a,b,n+1);
+h = (b-a)/n;
+y(:,1) = ybv;
+
+for i = 1:n
+    k1 = fun(t(i), y(:,i));
+    k2 = fun(t(i) + h, y(:,i) + h*k1);
+    y(:,1 + i) = y(:,i) + h*(k1+k2)/2;
+end
+
+end
