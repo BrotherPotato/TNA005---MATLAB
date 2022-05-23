748b5278b4aee336e8ae8b362a10989aaf6d701d
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-05-23 11:45:26 +0200

    s

--- MP3/angle.m
+++ MP3/angle.m
@@ -1,33 +1,14 @@
 function theta = angle(t) % mer perfekta vinklar, inte 0 och 800
-%theta
-%temp = sin(t);
-%temp = -2.18 + 0.00978 * t - 6.15 * 10^-6 * t.^2 - 5.84 * 10^-9 * t.^3;
 
-% y = zeros(1,length(t));
-% y(1:50) = pi;
-% y(50:125) = 3*pi/4;
-% y(125:400) = 5*pi/4;
-% y(400:675) = pi;
-% y(675:length(t)) = 3*pi/4;
-% 
-% theta = y;
-
-% if t < 50
-%     theta = 5*pi/4;
-% elseif t < 125
+% if t < 5
 %     theta = pi/2;
-% elseif t < 200
-%     theta = 3*pi/4;
-% elseif t < 400
+% elseif t < 8
 %     theta = 5*pi/4;
-% elseif t < 675
-%     theta = pi;
-% elseif t < 800
-%     theta = 3*pi/4;
-% else 
+% elseif t < 13
+%     theta = 3*pi/2;
+% else
 %     theta = pi;
 % end
-% theta;
 
 t;
 
@@ -48,26 +29,6 @@ elseif t < 25
 else 
     theta = 3*pi/2;
 end
-theta;
-
-
-% if temp >= pi/4 %temp <= 0 && 
-%     theta = 0;
-% elseif temp <= pi/4 && temp >= pi/2
-%     theta = pi/4;
-% elseif temp <= pi/2 && temp >= 3*pi/4
-%     theta = pi/2;
-% elseif temp <= 3*pi/4 && temp >= pi
-%     theta = 3*pi/4;
-% elseif temp <= pi && temp >= 5*pi/4
-%     theta = pi;
-% elseif temp <= 5*pi/4 && temp >= 3*pi/2
-%     theta = 5*pi/4;
-% elseif temp <= 3*pi/2 && temp >= 7*pi/4
-%     theta = 3*pi/2;
-% elseif temp <= 7*pi/4 %&& temp >= 2*pi
-%     theta = 7*pi/4;
-% end
-
+%theta;
 
 end
